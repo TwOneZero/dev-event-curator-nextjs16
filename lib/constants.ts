b33f45faf948c0187bc43dc9ebab056d455a734a
twonezero <dnjsdudtm98@naver.com>
2026-01-26 15:17:50 +0900

    feat(assets): add public images, icons and event constant data
    - public/icons 및 public/images 경로에 프로젝트용 아이콘 및 이미지 추가
    - lib/constants.ts에 이벤트 목록 상데이터(EVENTS) 정의
    - 기존 Next.js 기본 제공 svg 파일들(next.svg, vercel.svg 등) 제거

---
+++ lib/constants.ts
@@ -0,0 +1,59 @@
+export type EventItem = {
+  image: string
+  title: string
+  slug: string
+  location: string
+  date: string
+  time: string
+}
+
+export const events: EventItem[] = [
+  {
+    title: "Next.js Conf 2026",
+    image: "/images/event1.png",
+    slug: "nextjs-conf-2026",
+    location: "San Francisco, CA",
+    date: "2026-10-25",
+    time: "09:00 AM",
+  },
+  {
+    title: "React Summit",
+    image: "/images/event2.png",
+    slug: "react-summit",
+    location: "Amsterdam, NL",
+    date: "2026-06-12",
+    time: "10:00 AM",
+  },
+  {
+    title: "AWS re:Invent",
+    image: "/images/event3.png",
+    slug: "aws-reinvent",
+    location: "Las Vegas, NV",
+    date: "2026-12-01",
+    time: "08:30 AM",
+  },
+  {
+    title: "Google I/O 2026",
+    image: "/images/event4.png",
+    slug: "google-io-2026",
+    location: "Mountain View, CA",
+    date: "2026-05-20",
+    time: "11:00 AM",
+  },
+  {
+    title: "Web Summit 2026",
+    image: "/images/event5.png",
+    slug: "web-summit-2026",
+    location: "Lisbon, Portugal",
+    date: "2026-11-11",
+    time: "09:00 AM",
+  },
+  {
+    title: "JSWorld Conference",
+    image: "/images/event6.png",
+    slug: "jsworld-conference",
+    location: "Online",
+    date: "2026-02-15",
+    time: "02:00 PM",
+  },
+];
